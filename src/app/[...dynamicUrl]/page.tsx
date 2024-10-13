82bbadae1ef6c765cecf645a7d217ecd8bd83378
Isuru Nishadha <isurunishadha99@gmail.com>
2024-10-13 18:05:22 +0530

    reomved imports

--- src/app/[...dynamicUrl]/page.tsx
+++ src/app/[...dynamicUrl]/page.tsx
@@ -1,6 +1,6 @@
-import { ChatWrapper } from "@/components/ChatWrapper";
-import { ragChat } from "@/lib/rag-chat";
-import { redis } from "@/lib/redis";
+// import { ChatWrapper } from "@/components/ChatWrapper";
+// import { ragChat } from "@/lib/rag-chat";
+// import { redis } from "@/lib/redis";
 import { cookies } from "next/headers";
 
 interface PageProps {
