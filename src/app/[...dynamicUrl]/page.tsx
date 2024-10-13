7385ef75a7faabd7f321dc33daa13f85259a990a
Isuru Nishadha <isurunishadha99@gmail.com>
2024-10-13 18:52:26 +0530

    test

--- src/app/[...dynamicUrl]/page.tsx
+++ src/app/[...dynamicUrl]/page.tsx
@@ -1,7 +1,7 @@
-// import { ChatWrapper } from "@/components/ChatWrapper";
-// import { ragChat } from "@/lib/rag-chat";
-// import { redis } from "@/lib/redis";
-// import { cookies } from "next/headers";
+import { ChatWrapper } from "@/components/ChatWrapper";
+import { ragChat } from "@/lib/rag-chat";
+import { redis } from "@/lib/redis";
+import { cookies } from "next/headers";
 
 interface PageProps {
   params: {
@@ -15,36 +15,34 @@ function reconstructUrl({ url }: { url: string[] }) {
 }
 
 const Page = async ({ params }: PageProps) => {
-  // const sessionCookie = cookies().get("sessionId")?.value;
+  const sessionCookie = cookies().get("sessionId")?.value;
   const url = reconstructUrl({ url: params.dynamicUrl as string[] });
 
-  console.log("the constructued url = " + url);
+  const sessionId = (url + "--" + sessionCookie).replace(/\//g, "");
 
-  // const sessionId = (url + "--" + sessionCookie).replace(/\//g, "");
+  const isIndexed = await redis.sismember("indexed-urls", url);
 
-  // const isIndexed = await redis.sismember("indexed-urls", url);
+  const initialMessages = await ragChat.history.getMessages({
+    amount: 100,
+    sessionId,
+  });
 
-  // const initialMessages = await ragChat.history.getMessages({
-  //   amount: 100,
-  //   sessionId,
-  // });
+  if (!isIndexed) {
+    await ragChat.context.add({
+      type: "html",
+      source: url,
+      config: {
+        chunkOverlap: 50,
+        chunkSize: 200,
+      },
+    });
 
-  // if (!isIndexed) {
-  //   await ragChat.context.add({
-  //     type: "html",
-  //     source: url,
-  //     config: {
-  //       chunkOverlap: 50,
-  //       chunkSize: 200,
-  //     },
-  //   });
+    await redis.sadd("indexed-urls", url);
+  }
 
-  //   await redis.sadd("indexed-urls", url);
-  // }
-
-  // return (
-  //   <ChatWrapper sessionId={sessionId} initialMessages={initialMessages} />
-  // );
+  return (
+    <ChatWrapper sessionId={sessionId} initialMessages={initialMessages} />
+  );
 };
 
 export default Page;
